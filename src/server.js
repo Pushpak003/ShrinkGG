ce2c8072bb38713f983901bc25cfa392bc35d660
Pushpak003 <pushpakpathe63@gmail.com>
2025-09-08 23:33:03 +0530

    edited server file

--- src/server.js
+++ src/server.js
@@ -1,26 +1,9 @@
-import app from "./src/app.js";
-import pool from "./src/config/db.js";
+import app from "./app.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-
-(async () => {
-  try {
-    await pool.query(`
-      CREATE TABLE IF NOT EXISTS urls (
-        id SERIAL PRIMARY KEY,
-        original_url TEXT NOT NULL,
-        short_code VARCHAR(10) UNIQUE NOT NULL,
-        created_at TIMESTAMP DEFAULT NOW()
-      );
-    `);
-    console.log("✅ Database connected & table ready");
-  } catch (err) {
-    console.error("❌ Database error:", err);
-  }
-})();
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);

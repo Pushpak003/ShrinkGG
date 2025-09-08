0fa17eaa1e6fb784339eee2f980141a0b8afb3ad
Pushpak003 <pushpakpathe63@gmail.com>
2025-09-08 23:25:11 +0530

    database Configuration

--- src/config/db.js
+++ src/config/db.js
@@ -7,5 +7,13 @@ dotenv.config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
+(async () => {
+  try{
+    await pool.connect();
+    console.log("Connected to the database");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  }
+})();
 
 export default pool;

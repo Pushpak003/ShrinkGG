56726769bdc8495b85e8b8f752e8df98b83964d5
Pushpak003 <pushpakpathe63@gmail.com>
2025-09-08 23:30:50 +0530

    changed somethings

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from "express";
+import authRoutes from "./routes/authRoutes.js";
+import urlRoutes from "./routes/urlRoutes.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/url", urlRoutes);
+
+
 
 // Test route
 app.get("/", (req, res) => {
